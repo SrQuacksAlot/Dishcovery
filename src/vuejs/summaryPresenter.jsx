fbc8d3afd2768828837989c9143624bc122f73ce
Ante Abdullahi <anteab@kth.se>
2024-12-04 14:08:46 +0100

    cleaning up
    removed vuejs folder since not in use

--- src/vuejs/summaryPresenter.jsx
+++
@@ -1,7 +0,0 @@
-import { SummaryView } from "../views/summaryView.jsx";
-
-function Summary(props){
-    return <SummaryView people={props.model.numberOfGuests} ingredients={[] /* empty array for starters */}/>;
-}
-
-export { Summary }
