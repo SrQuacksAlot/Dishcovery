c88e4ea56782b03da3e2ba634878b50d549124f7
Khalil Khalil <kkhalil@kth.se>
2024-12-22 16:59:31 +0100

    removed uneccessarry stuff
    remvoed tests and tw

--- src/firebase/teacherFirebase.js
+++ src/firebase/teacherFirebase.js
@@ -1,16 +1,9 @@
-import * as x from "/test/mockFirebase.js";
+// import * as x from "/test/mockFirebase.js";
 import * as y from "firebase/database";
 
 /* Detects whether we are in a testing environment. If so, it returns a mock firebase, for testing. Otherwise it returns the real firebase. */
 
-let z;
-try{
-    mocha
-    z=x;
-}catch(e){
-    z=y;
-}
-const { getDatabase, ref, get, set, onValue, child, onChildAdded, onChildRemoved, off} =z;
+const { getDatabase, ref, get, set, onValue, child, onChildAdded, onChildRemoved, off} =y;
 export  { getDatabase, ref, get, set, onValue, child, onChildAdded, onChildRemoved, off};
 
 
